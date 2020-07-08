60580e22a11fbaee94af96feb79fa95c1cfecd06
DS-YIL <54430720+DS-YIL@users.noreply.github.com>
2020-07-08 10:01:24 +0530

    changes url

---
+++ src/app/pages/services/config.service.ts
@@ -0,0 +1,148 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { GroupMasterModel, AccessNameModel, AccessNameModelNew, AutrAuthorizationItemmodel, checkboxSelect } from '../../Models/config.model';
+import { GroupMastergetallModel, RoleNameModel } from '../../Models/config.model';
+import { GroupNameModel, AccessRoleModel } from '../../Models/config.model';
+import { RoleAccessModel } from '../../Models/config.model';
+import { constants } from '../../Models/MPRConstants'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ConfigService {
+  constructor(private http: HttpClient, private constants: constants) { }
+  public url = this.constants.url;
+  public httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) };
+
+  //All Method for GroupMaster
+
+  createNewGroupMaster(groupMasterModel: GroupMasterModel): Observable<any> {
+    const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) };
+    return this.http.post<any>(this.url + 'ConfigAccess/AddNewGroupMaster/', groupMasterModel, httpOptions);
+  }
+  getAllGroupMaster(): Observable<GroupMastergetallModel[]> {
+    return this.http.get<GroupMastergetallModel[]>(this.url + 'ConfigAccess/getAllGroupMaster');
+  }
+
+  deleteGroupMaster(groupmasterModel: GroupMastergetallModel): Observable<GroupMastergetallModel> {
+    const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) };
+    return this.http.put<GroupMastergetallModel>(this.url + 'ConfigAccess/DeleteGroupMaster', groupmasterModel, httpOptions);
+  }
+
+  updateGroupMaster(updategroupmasterModel: GroupMastergetallModel): Observable<GroupMastergetallModel> {
+    const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) };
+    return this.http.post<GroupMastergetallModel>(this.url + 'ConfigAccess/UpdateGroupMaster', updategroupmasterModel, httpOptions);
+  }
+
+  getAllGroupName(): Observable<GroupNameModel[]> {
+    return this.http.get<GroupNameModel[]>(this.url + 'ConfigAccess/getGroupNameDetail');
+  }
+
+
+
+
+  //All method for AccessNames
+
+  createAccessName(accessNameModel: AccessNameModelNew): Observable<any> {
+    const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) };
+    return this.http.post<any>(this.url + 'ConfigAccess/AddNewAccessName', accessNameModel, httpOptions);
+  }
+
+  getAccessName(): Observable<GroupMastergetallModel[]> {
+    return this.http.get<GroupMastergetallModel[]>(this.url + 'ConfigAccess/getAllGroupMaster');
+  }
+
+  deleteGroupAccess(accessNameModel: AccessNameModel): Observable<AccessNameModel> {
+    debugger
+    const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) };
+    return this.http.put<AccessNameModel>(this.url + 'ConfigAccess/DeleteGroupAccess', accessNameModel, httpOptions);
+  }
+
+  getAllAccessName(GroupName: string): Observable<GroupNameModel> {
+    return this.http.get<GroupNameModel>(this.url + 'ConfigAccess/getAccessName/' + GroupName);
+  }
+
+  //Update AccessName on cliclk of edit
+  updateAccessName(accessNameModel: AccessNameModelNew): Observable<AccessNameModelNew> {
+    const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) };
+    return this.http.post<AccessNameModelNew>(this.url + 'ConfigAccess/UpdateAccessName', accessNameModel, httpOptions);
+  }
+
+  //Delete AccessName on cliclk of edit
+  deleteAccessName(accessNameModel: AccessNameModel): Observable<AccessNameModel> {
+    const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) };
+    return this.http.post<AccessNameModel>(this.url + 'ConfigAccess/DeleteAccessName', accessNameModel, httpOptions);
+  }
+
+  //All Method for AuthGroup
+
+  createRoleAccess(roleAccessModel: RoleAccessModel): Observable<any> {
+    const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) };
+    return this.http.post<any>(this.url + 'ConfigAccess/AddNewRole/', roleAccessModel, httpOptions);
+  }
+
+  getAllRoleName(): Observable<RoleAccessModel[]> {
+    return this.http.get<RoleAccessModel[]>(this.url + 'ConfigAccess/getAllRole');
+  }
+
+
+
+  //All Method for Role Based Access Label
+
+  getAccessNamebyGroupId(accessGroupId: number): Observable<AccessNameModel[]> {
+    return this.http.get<AccessNameModel[]>(this.url + 'ConfigAccess/getAccessNameById' + '?accessGroupId=' + accessGroupId);
+  }
+  getAllNamebyId(roleId: number): Observable<AccessRoleModel[]> {
+    return this.http.get<AccessRoleModel[]>(this.url + 'ConfigAccess/getAllGroupById' + '?roleId=' + roleId);
+  }
+
+  getRoleName(): Observable<RoleNameModel[]> {
+    return this.http.get<RoleNameModel[]>(this.url + 'ConfigAccess/getAllRoleName');
+  }
+  getAuthItems(): Observable<any> {
+    return this.http.get<any>(this.url + 'ConfigAccess/getAuthorizationItemDetail');
+  }
+
+
+  addAccess(roleAccessModel: checkboxSelect): Observable<any[]> {
+    let params = new HttpParams();
+    const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) };
+    return this.http.post<any[]>(this.url + 'ConfigAccess/AddAccessRole', roleAccessModel, httpOptions);
+  }
+
+  getAccessNamebyAccessGroupId(accessGroupId: number): Observable<AccessNameModel[]> {
+    return this.http.get<AccessNameModel[]>(this.url + 'ConfigAccess/getGroupAccessNameById' + '?accessGroupId=' + accessGroupId);
+  }
+
+  getAccessNamebyId(accessGroupId: number): Observable<AccessNameModel[]> {
+    return this.http.get<AccessNameModel[]>(this.url + 'ConfigAccess/getAccessNameById' + '?accessGroupId=' + accessGroupId);
+  }
+
+  UpdateAccess(roleAccessModel: number[]): Observable<number[]> {
+    const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) };
+    return this.http.post<number[]>(this.url + 'ConfigAccess/UpdateAccess', roleAccessModel, httpOptions);
+  }
+
+  createAccess(roleAccessModel: RoleAccessModel): Observable<any> {
+    const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) };
+    return this.http.post<any>(this.url + 'ConfigAccess/AddAccess/', roleAccessModel, httpOptions);
+  }
+
+  //Update AuthRole on cliclk of edit
+  updateAuthRole(roleAccessModel: RoleAccessModel): Observable<RoleAccessModel> {
+    const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) };
+    return this.http.post<RoleAccessModel>(this.url + 'ConfigAccess/UpdateAuthRole', roleAccessModel, httpOptions);
+  }
+
+  //Delete AuthRole on cliclk of edit
+  deleteAuthRole(roleAccessModel: RoleAccessModel): Observable<RoleAccessModel> {
+    const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) };
+    return this.http.post<RoleAccessModel>(this.url + 'ConfigAccess/DeleteAuthRole', roleAccessModel, httpOptions);
+  }
+
+  getAllAccessNameData(): Observable<AccessNameModel[]> {
+    return this.http.get<AccessNameModel[]>(this.url + 'ConfigAccess/getAllAccessName');
+  }
+}
---
+++ src/app/pages/services/rfq.service .ts
@@ -0,0 +1,295 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, JsonpInterceptor } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
+import { Observable, BehaviorSubject } from 'rxjs';
+import { DynamicSearchResult, mprRevision, MPRItemInfoes, MPRDocument, MPRVendorDetail, MPRDocumentations, MPRStatusUpdate, mprFilterParams, MPRBuyerGroup, MPRApprovers, Vendor, Employee } from '../Models/mpr';
+import { constants } from '../Models/MPRConstants'
+import { RfqItemModel, rfqFilterParams } from '../Models/rfq';
+import { map } from 'rxjs/operators';
+import * as CryptoJS from 'crypto-js';
+@Injectable({
+	providedIn: 'root'
+})
+
+
+export class RfqService {
+	public tokenurl=this.constants.tokenurl;
+	public yscmurl=this.constants.yscmurl;
+	public AESkey:string;
+	
+	//private currentUserSubject: BehaviorSubject<Vendor>;
+	public accessToken:string;
+	public vendor:Vendor;
+	public tokenvalue:string;
+	//public currentUser:Observable<Vendor>
+	private currentUserSubject: BehaviorSubject<Employee>;
+	public currentUser: Observable<Employee>;
+	constructor(private http: HttpClient, private constants: constants) { 
+		if(localStorage.getItem('AccessToken'))
+		{
+		this.vendor=(JSON.parse(localStorage.getItem('AccessToken')));
+		this.accessToken=this.vendor.access_token;
+		}
+		//if(this.vendor)
+	
+		this.currentUserSubject=new BehaviorSubject<Employee>(JSON.parse(localStorage.getItem("vendordetail")));
+		this.currentUser=this.currentUserSubject.asObservable();
+	//}
+		
+	
+		
+	}
+	public get currentuservalue():Employee{
+		return this.currentUserSubject.value;
+	}
+	
+	
+	public url = this.constants.url;
+
+	gettoken(search: DynamicSearchResult) {
+		var data = "grant_type=password&username=" + search.username + "&password=" + search.password;
+		 const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/x-www-form-urlencoded' }) };
+		return this.http.post<any>(this.tokenurl + 'token',data,httpOptions)
+		  .pipe(map(data => {
+			if (data != null ||data!="") {
+			
+			  localStorage.setItem('AccessToken', JSON.stringify(data));
+			  this.currentUserSubject.next(data);
+			  this.vendor=(JSON.parse(localStorage.getItem('AccessToken')));
+			  if(this.vendor)
+			  this.accessToken=this.vendor.access_token;
+			}
+			return data;
+		  }
+		  )
+		  )
+		  
+		 
+	  }
+	  
+	  public httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json','Authorization': "bearer " + this.accessToken}) };
+	getRFQItems(MPRRevisionId: number): Observable<any> {
+		const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json','Authorization':"bearer "+this.accessToken }) };
+		return this.http.get<any>(this.url + 'RFQ/getRFQItems/' + MPRRevisionId,httpOptions);
+	}
+	logout() {
+		localStorage.clear();
+		this.currentUserSubject.next(null);
+		
+	  }
+	
+	updateVendorQuotes(vendorQuoteList: any, termsList: any): Observable<any> {
+		const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json','Authorization':"bearer "+this.accessToken }) };
+		var Data = {
+			RFQQuoteViewList: vendorQuoteList,
+			TermsList: termsList
+		}
+		return this.http.post<any>(this.url + 'RFQ/updateVendorQuotes', JSON.stringify(Data), httpOptions);
+	}
+	getRFQCompareItems(MPRRevisionId: number): Observable<any> {
+		const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json','Authorization':"bearer "+this.accessToken }) };
+		return this.http.get<any>(this.url + 'RFQ/getRFQCompareItems/' + MPRRevisionId,httpOptions);
+	}
+	
+	statusUpdate(vendorList: any): Observable<any> {
+		const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json','Authorization':"bearer "+this.accessToken }) };
+		var Data = {
+			RFQQuoteViewList: vendorList
+		}
+		return this.http.post<any>(this.url + 'RFQ/rfqStatusUpdate/', JSON.stringify(Data), httpOptions);
+	}
+	GetItemsByRevisionId(RevisionId: number): Observable<any> {
+		const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json','Authorization':"bearer "+this.accessToken }) };
+		return this.http.get<any>(this.url + 'RFQ/GetItemsByRevisionId/' + RevisionId,httpOptions);
+	}
+	GetRfqDetailsById(RevisionId: number): Observable<any> {
+		const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json','Authorization':"bearer "+this.accessToken }) };
+		return this.http.get<any>(this.url + 'RFQ/GetRfqDetailsById/' + RevisionId,httpOptions);
+	}
+	GetdocDetailsById(RevisionId: number): Observable<any> {
+		const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json','Authorization':"bearer "+this.accessToken }) };
+		return this.http.get<any>(this.url + 'RFQ/getdocumentbyrevid/' + RevisionId,httpOptions);
+	}
+	GetdocDetailsByIdanditemsid(RevisionId: number,rfqitemsid:number): Observable<any> {
+		const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json','Authorization':"bearer "+this.accessToken }) };
+		return this.http.get<any>(this.url + 'RFQ/getdocumentbyrevid/' + RevisionId+"/"+rfqitemsid,httpOptions);
+	}
+	//this
+	GetUnitMasterList(): Observable<any> {
+		const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json','Authorization':"bearer "+this.accessToken }) };
+		return this.http.get<any>(this.url + 'RFQ/GetUnitMasterList', httpOptions);
+	}
+	GetAllMasterCurrency(): Observable<any> {
+		const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json','Authorization':"bearer "+this.accessToken }) };
+		return this.http.get<any>(this.url + 'RFQ/GetAllMasterCurrency', httpOptions);
+	}
+	InsertRfqItemInfo(rfqItem: RfqItemModel): Observable<any> {
+		const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json', 'Authorization':"bearer "+this.accessToken}) };
+		return this.http.post<any>(this.url + 'RFQ/InsertRfqItemInfo/', rfqItem, httpOptions);
+	}
+	editRfqItemInfo(rfqItem: RfqItemModel): Observable<any> {
+		const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json','Authorization':"bearer "+this.accessToken }) };
+		return this.http.post<any>(this.url + 'RFQ/editRfqItemInfo/', rfqItem, httpOptions);
+	}
+	
+	getRFQList(rfqFilterParams: rfqFilterParams): Observable<any[]> {
+		const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json','Authorization':"bearer "+this.accessToken }) };
+		return this.http.post<any[]>(this.url + 'RFQ/getRFQList/', rfqFilterParams, httpOptions);
+	}
+	GetRfqByVendorId(VendorId: any): Observable<any> {
+		const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json','Authorization':"bearer "+this.accessToken }) };
+		return this.http.post<any>(this.url + 'RFQ/GetRfqByVendorId/', JSON.stringify(VendorId),httpOptions);
+	}
+	//getallrfqlist(): Observable<any> {
+	//	return this.http.get<any>(this.url + 'RFQ/getallrfqlist', this.httpOptions);
+	//}
+	VendorregisterSubmit(vendorList: any): Observable<any> {
+		const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json','Authorization':"bearer "+this.accessToken }) };
+		return this.http.post<any>(this.url + 'RFQ/VendorRegister/', JSON.stringify(vendorList), httpOptions);
+	}
+	VendorTermsUpdate(vendorTermsList: any): Observable<any[]> {
+		const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json','Authorization':"bearer "+this.accessToken }) };
+		return this.http.post<any[]>(this.url + 'RFQ/RFQTermUpdate/', JSON.stringify(vendorTermsList), httpOptions);
+	}
+	FinalSumit(RFQRevisionId: number): Observable<any[]> {
+		const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json','Authorization':"bearer "+this.accessToken }) };
+		return this.http.get<any>(this.url + 'RFQ/finalsubmitfromVendor/'+RFQRevisionId, httpOptions);
+	}
+	VendorregisterSave(vendorList: any): Observable<any> {
+		const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json','Authorization':"bearer "+this.accessToken }) };
+		return this.http.post<any>(this.url + 'RFQ/SaveVendorDetails/', JSON.stringify(vendorList), httpOptions);
+	}
+	getvendordetails(vendorid: number): Observable<any> {
+		const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json','Authorization':"bearer "+this.accessToken }) };
+		return this.http.get<any>(this.url + 'RFQ/Getvendordetails/' +vendorid, httpOptions);
+	}
+	uploadFile(formdata: FormData): Observable<any> {
+		const customHeaders = new HttpHeaders({
+			'Authorization': 'Bearer ' + this.accessToken,
+			'Accepted-Encoding': 'application/json'
+		  });
+		  const customOptions = {
+			headers: customHeaders,
+			reportProgress: true,
+		  };
+		const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json','Authorization':"bearer "+this.accessToken }) };
+		return this.http.post<any>(this.url + 'RFQ/UploadFile/', formdata,customOptions)
+		  .pipe(map(data => {
+			return data;
+		  }))
+	  }
+	  GetStateNames(): Observable<any> {
+		const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json','Authorization':"bearer "+this.accessToken }) };
+		return this.http.get<any>(this.url + 'RFQ/GetStateList', httpOptions);
+	}
+	GetTermsListForRFQ(RFQRevisionId:number): Observable<any> {
+		const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json','Authorization':"bearer "+this.accessToken }) };
+		return this.http.get<any>(this.url + 'RFQ/GetAllRFQTerms/'+RFQRevisionId, httpOptions);
+	}
+	GetNatureofBusinessList(): Observable<any> {
+		const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json','Authorization':"bearer "+this.accessToken }) };
+		return this.http.get<any>(this.url + 'RFQ/GetNaturOfBusiness', httpOptions);
+	}
+	GetDocumentNames(): Observable<any> {
+		const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json','Authorization':"bearer "+this.accessToken }) };
+		return this.http.get<any>(this.url + 'RFQ/GetDocumentTypeList', httpOptions);
+	}
+	GetDocumentMasterList(): Observable<any> {
+		const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json','Authorization':"bearer "+this.accessToken }) };
+		return this.http.get<any>(this.url + 'RFQ/GetDocumentMasterList', httpOptions);
+	}
+	DeleteFile(vendorList: any): Observable<any> {
+		const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json','Authorization':"bearer "+this.accessToken }) };
+		return this.http.post<any>(this.url + 'RFQ/deleteAttachedDocuments/', JSON.stringify(vendorList), httpOptions);
+	}
+	changepassword(vendorList: any): Observable<any> {
+		const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json','Authorization':"bearer "+this.accessToken }) };
+		return this.http.post<any>(this.url + 'RFQ/changepassword/', JSON.stringify(vendorList), httpOptions);
+	}
+	DeleteFileFrmYSCM(mprdocument: any): Observable<any> {
+		const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json','Authorization':"bearer "+this.accessToken }) };
+		return this.http.post<any>(this.yscmurl + 'deleteMPRDocument/', JSON.stringify(mprdocument));
+	}
+	DeleteDocuments(vendorList: any): Observable<any> {
+		const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json','Authorization':"bearer "+this.accessToken }) };
+		
+		return this.http.post<any>(this.url + 'RFQ/deleteAttachedfile/', JSON.stringify(vendorList), httpOptions);
+	}
+	InsertDocument(formData: FormData): Observable<any> {
+		const customHeaders = new HttpHeaders({
+			'Authorization': 'Bearer ' + this.accessToken,
+			'Accepted-Encoding': 'application/json'
+		  });
+		  const customOptions = {
+			headers: customHeaders,
+			reportProgress: true,
+		  };
+		const httpOptions = { headers: new HttpHeaders({'Accept':'application/json', 'Content-Type': 'multipart/form-data','Authorization':"bearer "+this.accessToken }) };
+		return this.http.post<any>(this.url + 'RFQ/InsertDocument/', formData,customOptions)
+		
+		  .pipe(map(data => { return data }));
+	  }
+	  InsertDocumentToYSCM(formData: FormData): Observable<any> {
+		return this.http.post<any>(this.yscmurl +'uploadfile/', formData)
+		
+		  .pipe(map(data => { return data }));
+	  }
+	 
+	  InsertVendorCommunication(vendorlist: any): Observable<any> {
+		const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json','Authorization':"bearer "+this.accessToken }) };
+		return this.http.post<any>(this.url + 'RFQ/InsertVendorCommunication/', JSON.stringify(vendorlist), httpOptions)
+		  .pipe(map(data => { return data }));
+	  }
+	  RfqIteminfoDeleteByid(id: number,RFQItemsId:number): Observable<any> {
+		const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json','Authorization':"bearer "+this.accessToken }) };
+		return this.http.get<any>(this.url + 'RFQ/RfqIteminfoDeleteByid/'+id +'/'+RFQItemsId, httpOptions)
+		.pipe(map(data => { return data }));
+	  }
+	  RfqIteminfoDeleteByidformultiple(rfqitemid: number,bomid:number): Observable<any> {
+		const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json','Authorization':"bearer "+this.accessToken }) };
+		return this.http.get<any>(this.url + 'RFQ/RfqIteminfoDeleteByidformultiple/'+rfqitemid +'/' +bomid, httpOptions)
+		.pipe(map(data => { return data }));
+	  }
+	  GetVendorCommunicationForRFQRevId(RFQRevisionId: number): Observable<any> {
+		const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json','Authorization':"bearer "+this.accessToken }) };
+		return this.http.get<any>(this.url + 'RFQ/GetCommunicationListForRFQRevisionId/'+RFQRevisionId, httpOptions)
+		  .pipe(map(data => { return data }));
+	  }
+	
+	  gettermsandconditionsByid(RFQRevisionId: number): Observable<any> {
+		const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json','Authorization':"bearer "+this.accessToken }) };
+		return this.http.get<any>(this.url + 'RFQ/GetTermsByRFQrevisionId/'+RFQRevisionId, httpOptions);
+	  }
+	  
+	 getVendordetail(accesstoken:any): Observable<any> {
+		const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json','Authorization':"bearer "+accesstoken }) };
+
+		return this.http.get<any>(this.url + 'RFQ/Vendordetails',httpOptions)
+		.pipe(map(data=>{
+			if(data!=null){
+				localStorage.setItem('vendordetail', JSON.stringify(data));
+				this.currentUserSubject.next(data);
+				
+				return data;
+			}
+		}));
+
+	  }
+	  checkemail(emailid:any): Observable<any> {
+		const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) };
+		return this.http.post<any>(this.url + 'Forgetpassword/checkemail/',emailid,httpOptions);
+	}
+	sendEmailLink(emailid:any): Observable<any> {
+		const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) };
+		return this.http.post<any>(this.url + 'Forgetpassword/sendLinkforForgetpassword/',emailid,httpOptions);
+	}
+	Resetpassword(vendorList: any): Observable<any> {
+		const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json'}) };
+		return this.http.post<any>(this.url + 'Forgetpassword/Resetpassword/', JSON.stringify(vendorList), httpOptions);
+	}
+	checklinkexpired(vendorList: any): Observable<any> {
+		const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json'}) };
+		return this.http.post<any>(this.url + 'Forgetpassword/checkforgetpasswordlink/', JSON.stringify(vendorList), httpOptions);
+	}
+			  
+}
